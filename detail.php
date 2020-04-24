424fbb4e9ec354123cfcc0b4520fd7e358cd905b
Victor <ojedavd@gmail.com>
2020-04-24 16:24:33 -0300

    Cambios en detail

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setAccessToken('APP_USR-7455310476774640-042418-208e377607b9c1e908f72d0c17be8715-469485398');
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
