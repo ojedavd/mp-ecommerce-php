044d3c90893e7bcf0d15e8ff8492afc8265fa6ce
Victor <ojedavd@gmail.com>
2020-04-24 17:05:37 -0300

    Cambios

--- detail.php
+++ detail.php
@@ -2,13 +2,10 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
-// Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-// Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
-// Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
 $item->id = '1234';
 $item->title = $_POST['title'];
